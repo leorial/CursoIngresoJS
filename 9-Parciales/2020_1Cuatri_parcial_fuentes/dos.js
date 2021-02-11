6a2fa490311933a1a1e4505d9475a252b7a5f8d0
LeoRial <leonardo.rial@hotmail.com>
2021-02-11 20:28:07 -0300

    pacial 2020 punto 2 y 3

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -13,6 +13,8 @@ function mostrar()
   let y;
   let ce;
   let z;
+  let bolsastotal;
+  let preciototal;
 
  
   cantidadarena=0;
@@ -21,6 +23,7 @@ function mostrar()
   preciocemento=0;
   preciocal=0;
   precioarena=0;
+  bolsastotal=0;
   
   
   for(; ;)
@@ -38,7 +41,7 @@ function mostrar()
         x=parseFloat(x);
        if (x>0)
         {
-          precioarena=(precioarena+x)*a;
+          precioarena=precioarena+(x*a);
         }
        break;
 
@@ -50,7 +53,7 @@ function mostrar()
         y=parseFloat(y)
        if (y>0)
        {
-         preciocal=(preciocal+y)*ca;
+         preciocal=preciocal+(y*ca);
        }
        break;
 
@@ -62,24 +65,59 @@ function mostrar()
         z=parseFloat(z);
        if (z>0)
        {
-        preciocemento=(preciocemento+z)*ce;
+        preciocemento=preciocemento+(z*ce);
        }
        break;
     }
-
-
-
-
-
-
+  
     seguir=prompt("desea ingresar otro articulo? s/n");
     if (seguir=="n")
     {
       break;
     }
   }
-alert(cantidadcemento);
-alert(preciocal);
-alert(cantidadarena);
+
+bolsastotal=cantidadcemento+cantidadcal+cantidadarena;
+preciototal=preciocemento+preciocal+precioarena;
+
+alert("el importe a pagar bruto es: "+preciototal);
+
+if (bolsastotal>30)
+{
+  preciototal=preciototal-(preciototal*25/100);
+  alert("precio total con 25% de descuento: "+preciototal);
+}
+else if (bolsastotal>10)
+{
+  preciototal=preciototal-(preciototal*15/100);
+  alert("precio total con 15% de descuento: "+preciototal);
+}
+
+
+if (cantidadcemento>cantidadarena && cantidadcemento>cantidadcal)
+{
+  alert("hay mas catidad de bolsas de cemento");
+}
+else if (cantidadcal>cantidadarena && cantidadcal>cantidadcemento)
+{
+  alert("hay mas cantidad de bolsas de cemento");
+}
+else
+{
+  alert("hay mas cantidad de bolsas de arena");
+}
+
+if (preciocemento>preciocal && preciocemento>precioarena)
+{
+  alert("las bolsas de cemento son las mas caras");
+}
+else if (preciocal>preciocemento && preciocal>precioarena)
+{
+  alert("las bolsas de cal son las mas caras");
+}
+else
+{
+  alert("las bolsas de arena son las mas caras");
+}
 
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,4 +1,81 @@
 function mostrar()
 {
-	alert("tres");
+	let nombre;
+	let edad;
+	let sexo;
+	let estadocivil;
+	let temperatura;
+	let t; // mayor temperatura
+	let n; //nombre con mas temperatura
+	let v; //hombre mayores de edad viudos
+	let h; //cantidad de hombre solteros o viudos
+	let x; //ancianos con mas de 38 grados
+	let s; //cantidad de hombres
+	let e; //edad de hombres
+	let promedio;
+
+	t=0;
+	v=0;
+	h=0;
+	x=0;
+	s=0;
+	e=0;
+
+
+	for (; ;)
+	{
+		nombre=prompt("ingrese nombre:");
+		
+		edad=parseInt(prompt("ingrese edad:"));
+
+
+		sexo=prompt("ingrese sexo: m/f");
+		
+		estadocivil=prompt("ingrese estado civil: soltero/casado/viudo");
+
+		temperatura=parseInt(prompt("ingrese temperatura en grados:"));
+		if (temperatura>t)
+		{
+			t=temperatura;
+			n=nombre;
+		}
+
+		if (temperatura>38 && edad>60)
+		{
+			x++;
+		}
+
+		if (sexo=="m" && estadocivil=="soltero")
+		{
+			s++;
+			e=e+edad;
+		}
+
+		if (estadocivil == "viudo" && edad>=18)
+		{
+			v++;
+		}
+
+		if (estadocivil != "casado" && sexo=="m")
+		{
+			h++
+		}
+
+		
+		let seguir=prompt("desea ingresar otro pasajero? s/n");
+		if (seguir != "s")
+		{
+			break;
+		}
+	}
+	promedio=e/s;
+	alert("la persona con mas temperatura es: "+n);
+	alert("hay "+v+" mayores de edad viudos");
+	alert("hay "+h+" soltero o viudos");
+	alert("hay "+x+" mayores de edad con mas de 38 grados");
+	alert("hla edad promedio de hombre solteros es de "+promedio);
+
+
+
+
 }
